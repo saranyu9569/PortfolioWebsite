c57c45116713fcb02090d8d5ae3612b737c13b41
Genki_st <97723258+saranyu9569@users.noreply.github.com>
2024-11-30 23:34:36 +0700

    Update SkillSection.jsx
    Fix the Expertise part make it seperate into 2 column

--- website/src/app/components/SkillSection/SkillSection.jsx
+++ website/src/app/components/SkillSection/SkillSection.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function SkillSection() {
   const skills = [
-    { name: 'Python', percentage: 100 },
-    { name: 'Java', percentage: 100 },
-    { name: 'C', percentage: 95 },
-    { name: 'Javascript', percentage: 90 },
-    { name: 'Prolog', percentage: 90 },
-    { name: 'C++', percentage: 85 },
-    { name: 'C#', percentage: 85 },
-    { name: 'Typescript', percentage: 75 },
+    { name: "Python", percentage: 100 },
+    { name: "Java", percentage: 100 },
+    { name: "C", percentage: 95 },
+    { name: "Javascript", percentage: 90 },
+    { name: "Prolog", percentage: 90 },
+    { name: "C++", percentage: 85 },
+    { name: "C#", percentage: 85 },
+    { name: "Typescript", percentage: 75 },
   ];
 
   return (
     <div className="p-24 mt-10">
-      <h2 className="text-3xl font-semibold mb-2 lg:text-5xl xl:text-5xl">My Skills</h2>
-      <div className='border border-2 w-2/5 mb-6 md:w-1/5'/>
+      <h2 className="text-3xl font-semibold mb-2 lg:text-5xl xl:text-5xl">
+        My Skills
+      </h2>
+      <div className="border border-2 w-2/5 mb-6 md:w-1/5" />
       <div className="mt-8">
-        <h1 className="text-xl font-medium font-bold"><strong className='text-2xl'>Knowledge about</strong>: Machine Learning / Deep Learning, Web Development, Internet of Things (IOT)</h1>
+        <h1 className="text-xl font-medium font-bold">
+          <strong className="text-3xl">Expertise</strong>
+          <div className="grid grid-cols-2 gap-4 ml-10 pt-3 text-2xl font-semibold">
+            <li>Machine Learning</li>
+            <li>Deep Learning</li>
+            <li>Computer Vision</li>
+            <li>Internet of Things (IOT)</li>
+            <li>Cyber Security</li>
+            <li>Web Development</li>
+          </div>
+        </h1>
       </div>
 
       {/* Grid Layout for the skills */}
@@ -28,13 +40,21 @@ function SkillSection() {
               <div className="relative pt-1">
                 <div className="flex mb-2 items-center justify-between">
                   <div>
-                    <span className="text-lg font-semibold">{skill.name} ({skill.percentage}%)</span>
+                    <span className="text-lg font-semibold">
+                      {skill.name} ({skill.percentage}%)
+                    </span>
                   </div>
                 </div>
                 <div className="flex mb-2">
                   <div className="w-full bg-gray-200 rounded-full">
                     <div
-                      className={`h-2 rounded-full ${skill.percentage === 100 ? 'bg-green-600' : skill.percentage >= 75 ? 'bg-yellow-500' : 'bg-red-500'}`}
+                      className={`h-2 rounded-full ${
+                        skill.percentage === 100
+                          ? "bg-green-600"
+                          : skill.percentage >= 75
+                          ? "bg-yellow-500"
+                          : "bg-red-500"
+                      }`}
                       style={{ width: `${skill.percentage}%` }}
                     ></div>
                   </div>
@@ -45,7 +65,7 @@ function SkillSection() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default SkillSection;
